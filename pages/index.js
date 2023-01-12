cd405f2947b3a368ce129f02f4705c65a6203b97
Jeffrey Ding <jeffreyding18@gmail.com>
2023-01-11 16:21:25 -0800

    v 0.0

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Test title!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Test text <code>pages/index.js</code>
         </p>
       </main>
 
